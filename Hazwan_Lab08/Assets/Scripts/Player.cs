63dcf346dfe09820720b9efa0c439459508da943
28_Hazwan <114890871+28-Hazwan@users.noreply.github.com>
2022-10-26 11:22:54 +0800

    Complete

---
+++ Hazwan_Lab08/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public GameObject text;
+    public float score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.GetComponent<Text>().text = "Score : " + score;
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+      
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+        if (collision.gameObject.tag == "Score")
+        {
+            score++;
+        }
+    }
+}
---
+++ Hazwan_Lab08/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObject;
+    float PositionY;
+    public float interval;
+    public float interval2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", interval2, interval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(SpawnObject, transform.position, transform.rotation);
+    }
+}
